f1c152d21b3ea8a5afba30d5ad860fbedf4849d5
birchmd <birchmd@casperlabs.io>
2019-10-24 16:17:23 -0400

    NO-TICKET: Fix conversion of Key into ContractPointer in other contract example calls, required after store_function changed to create a uref instead of a hash

--- counter-call/src/lib.rs
+++ counter-call/src/lib.rs
@@ -6,17 +6,13 @@ extern crate contract_ffi;
 
 use alloc::vec::Vec;
 
-use contract_ffi::contract_api::{runtime, ContractRef, Error};
-use contract_ffi::key::Key;
+use contract_ffi::contract_api::{runtime, Error};
 use contract_ffi::unwrap_or_revert::UnwrapOrRevert;
 
 #[no_mangle]
 pub extern "C" fn call() {
     let contract_key = runtime::get_key("counter").unwrap_or_revert_with(Error::GetKey);
-    let pointer = match contract_key {
-        Key::Hash(hash) => ContractRef::Hash(hash),
-        _ => runtime::revert(Error::UnexpectedKeyVariant),
-    };
+    let pointer = contract_key.to_c_ptr().unwrap_or_revert_with(Error::UnexpectedKeyVariant);
 
     let arg = ("inc",);
     runtime::call_contract::<_, ()>(pointer.clone(), &arg, &Vec::new());
--- mailing-list-call/src/lib.rs
+++ mailing-list-call/src/lib.rs
@@ -8,17 +8,14 @@ extern crate contract_ffi;
 use alloc::string::String;
 use alloc::vec::Vec;
 
-use contract_ffi::contract_api::{runtime, storage, ContractRef, Error, TURef};
+use contract_ffi::contract_api::{runtime, storage, Error, TURef};
 use contract_ffi::key::Key;
 use contract_ffi::unwrap_or_revert::UnwrapOrRevert;
 
 #[no_mangle]
 pub extern "C" fn call() {
     let contract_key = runtime::get_key("mailing").unwrap_or_revert_with(Error::GetKey);
-    let pointer = match contract_key {
-        Key::Hash(hash) => ContractRef::Hash(hash),
-        _ => runtime::revert(Error::UnexpectedKeyVariant),
-    };
+    let pointer = contract_key.to_c_ptr().unwrap_or_revert_with(Error::UnexpectedKeyVariant);
 
     let method = "sub";
     let name = "CasperLabs";
